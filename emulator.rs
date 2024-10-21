00a51f2856b9b1eb4a12cf491f1a201a285163fa
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-10-21 17:45:52 -0500

    cleaning up bitfields, CPU intialization

--- emulator.rs
+++ emulator.rs
@@ -1,6 +1,20 @@
 
 // Data Sheet on the 6502
 // https://web.archive.org/web/20210909190432/http://www.obelisk.me.uk/6502/
+
+
+//     let mut bit_field = BitField::new(4);
+//     bit_field.set_bit(0,1);
+//     bit_field.set_bit(1,0);
+//     bit_field.set_bit(2,1);
+//     bit_field.set_bit(3,1);
+//
+//     for i in 0..4 {
+//         println!("Bit {} is: {}", i, bit_field.get_bit(i));
+//     }
+
+
+
 struct BitField {
     data: u8,
     size: usize,
@@ -12,39 +26,86 @@ impl BitField {
         BitField { data: 0, size}
     }
 
-    fn set_bit(&mut self, index: usize, value: bool) {
-        if value {
-            self.data |= 1 <<index;
+    fn set_bit(&mut self, index: usize, value: u8) {
+        if index >= self.size {
+            panic!("Index out of bounds");
+        }
+        if value == 1 {
+            self.data |= 1 <<index; // Set the bit at 'index'
+        } else if value == 0 {
+            self.data &= !(1 << index); // Clear the bit at 'index'
         } else {
-            self.data &= !(1 << index);
+            panic!("Value must be 0 or 1");
         }
     }
 
-    fn get_bit(&self, index: usize) -> bool {
-        (self.data & (1<<index)) != 0
+    fn get_bit(&self, index: usize) -> u8 {
+        if (self.data & (1 << index)) !=0 {
+            1
+        } else {
+            0
+        }
     }
-}
 
+    fn data(&self) -> u8 {
+        self.data
+    }
+} 
 
 type Byte = u8;
 type Word = u16;
 
-
 struct CPU {
-    PC: Word, //Program Counter
-    SP: Word, //Stack Pointer
+    PC: Word, // Program Counter
+    SP: Word, // Stack Pointer
 
-    //Register
-    A: Byte, //Accumulator Register
-    X: Byte, //Register X
-    Y: Byte, //Register Y
+    // Register
+    A: Byte, // Accumulator Register
+    X: Byte, // Register X
+    Y: Byte, // Register Y
 
+    // Status Flags (all are 1 bit) 
+    C: bool, // Carry Flag
+    Z: bool, // Zero Flag
+    I: bool, // Interrupt Disable
+    D: bool, // Decimal Mode
+    B: bool, // Break Command
+    V: bool, // Overflow Flag
+    N: bool, // Negative Flag
 
+    fn Reset() -> () {
+        // Pointers
+        let PC = 0xFFFC;
+        let SP = 0x0100;
 
+        // Clearing Registers
+        let A = 0;
+        let X = 0;
+        let Y = 0;
 
+        // Clearing Status Flags
+        let C = false;
+        let Z = false;
+        let I = false;
+        let D = false;
+        let B = false;
+        let V = false;
+        let N = false;
+    }
 };
 
 fn main () {
 
+    let mut cpu = CPU;
     return 0;
 }
+
+//     let mut bit_field = BitField::new(4);
+//     bit_field.set_bit(0,1);
+//     bit_field.set_bit(1,0);
+//     bit_field.set_bit(2,1);
+//     bit_field.set_bit(3,1);
+//
+//     for i in 0..4 {
+//         println!("Bit {} is: {}", i, bit_field.get_bit(i));
+//     }
